4e0af951b90e25c71ff3ad4f264dc877ec3008a4
cayden.ngai <cayden.ngai@lalamove.com>
2022-08-09 11:44:25 +0800

    Modified the .gitignore file and Added the lib folder

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\n Same Object: " + result);
+		System.out.println("\n Memory location for the Coach: " + theCoach);
+		System.out.println("\n Memory location for the alphaCoach: " + alphaCoach);
+		
+		// close the context
+		context.close();
+	}
+
+}
